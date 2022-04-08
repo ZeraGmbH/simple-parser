037be5fad88c333a2869258bf107fe0136068ee7
Andreas Müller <schnitzeltony@gmail.com>
2022-04-08 20:39:41 +0200

    Follow clazy and replace QStringLiteral() by QLatin1String() for empty strting
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- src/simplecmdiobase/cmdclient.cpp
+++ src/simplecmdiobase/cmdclient.cpp
@@ -14,7 +14,7 @@ void QSimpleCmdClient::OnReceive()
     QTcpSocket *pSocket = qobject_cast<QTcpSocket*>(sender());
     if(pSocket == m_pSocket) {
         QString ReadData = QString::fromUtf8(m_pSocket->readAll());
-        ReadData = ReadData.replace(END_IGNORE, QStringLiteral());
+        ReadData = ReadData.replace(END_IGNORE, QLatin1String());
         m_strCmdBuffer += ReadData;
         int iLFPos;
         while((iLFPos=m_strCmdBuffer.indexOf(END_STR)) >= 0) {
