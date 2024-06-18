2de7f0b1203a68b87238c5c4d1019b08afeef899
Andreas Müller <schnitzeltony@gmail.com>
2024-06-18 20:18:14 +0200

    Add draft of SimpleCmdIoClient
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ src/simplecmdio-client/lib/simplecmdioclient.cpp
@@ -0,0 +1,11 @@
+#include "simplecmdioclient.h"
+
+SimpleCmdIoClient::SimpleCmdIoClient(int portNo)
+    : m_portNo(portNo)
+{
+}
+
+void SimpleCmdIoClient::startCmd(QString cmd)
+{
+
+}
---
+++ src/simplecmdio-client/lib/simplecmdioclient.h
@@ -0,0 +1,18 @@
+#ifndef SIMPLECMDIOCLIENT_H
+#define SIMPLECMDIOCLIENT_H
+
+#include <QObject>
+
+class SimpleCmdIoClient : public QObject
+{
+    Q_OBJECT
+public:
+    explicit SimpleCmdIoClient(int portNo);
+    void startCmd(QString cmd);
+signals:
+    void sigCmdFinish(bool ok, QString returnedString);
+private:
+    int m_portNo;
+};
+
+#endif // SIMPLECMDIOCLIENT_H
