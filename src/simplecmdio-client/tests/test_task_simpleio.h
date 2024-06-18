733f94795de065ada72a6db01eccc57b785096de
Andreas Müller <schnitzeltony@gmail.com>
2024-06-18 20:14:15 +0200

    Introduce TaskSimpleSendReceive
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ src/simplecmdio-client/tests/test_task_simpleio.h
@@ -0,0 +1,29 @@
+#ifndef TEST_TASK_SIMPLEIO_H
+#define TEST_TASK_SIMPLEIO_H
+
+#include "testcmdiofacade.h"
+#include <QObject>
+#include <memory>
+
+// This test tests simplecmdio-testlib too which was created along with
+// SimpleCmdIoClient. Maybe we think about a more simple server test
+// implementation once SimpleCmdIoClient has matured...
+
+class test_task_simpleio : public QObject
+{
+    Q_OBJECT
+private slots:
+    void init();
+    void cleanup();
+    void failInvalidSocket();
+    void failInvalidCommand();
+    void passCommand();
+    void failErrorMessageSet();
+private:
+    void setupServer();
+    void setupOpenClient();
+    std::unique_ptr<TestCmdIoFacade> m_simpleServer;
+    std::shared_ptr<QTcpSocket> m_clientSocket;
+};
+
+#endif // TEST_TASK_SIMPLEIO_H
