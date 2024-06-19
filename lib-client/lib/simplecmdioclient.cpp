5447a559c0de729cb4d4e563d3dd7ff284d22f23
mdoerr2s <mika.doerr@smail.inf.h-brs.de>
2024-06-19 11:10:27 +0200

    SimpleCmdIoClient: Add test and functionality to send commands
    Signed-off-by: mdoerr2s <mika.doerr@smail.inf.h-brs.de>

--- lib-client/lib/simplecmdioclient.cpp
+++ lib-client/lib/simplecmdioclient.cpp
@@ -1,11 +1,34 @@
 #include "simplecmdioclient.h"
+#include "taskopenconnection.h"
+#include "tasksimplesendreceive.h"
+#include "tasklambdarunner.h"
 
-SimpleCmdIoClient::SimpleCmdIoClient(int portNo)
-    : m_portNo(portNo)
+SimpleCmdIoClient::SimpleCmdIoClient(QString ip, int portNo, int timeout) :
+    m_ip(ip),
+    m_portNo(portNo),
+    m_timeout(timeout)
 {
 }
 
 void SimpleCmdIoClient::startCmd(QString cmd)
 {
+    m_socket = std::make_shared<QTcpSocket>();
+    m_tasks = TaskContainerSequence::create();
 
+    TaskTemplatePtr openConnection = TaskOpenConnection::create(m_socket, m_ip, m_portNo, m_timeout);
+    m_tasks->addSub(std::move(openConnection));
+
+    TaskTemplatePtr sendCmd = TaskSimpleSendReceive::create(m_socket, cmd, m_timeout);
+    m_tasks->addSub(std::move(sendCmd));
+
+    TaskTemplatePtr closeConnection = TaskLambdaRunner::create([this]() {
+        m_socket->close();
+        return true;
+    });
+    m_tasks->addSub(std::move(closeConnection));
+
+    connect(m_tasks.get(), &TaskContainerSequence::sigFinish,
+            this, &SimpleCmdIoClient::sigCmdFinish);
+
+    m_tasks->start();
 }
---
+++ lib-client/tests/test_simplecmdioclient.cpp
@@ -0,0 +1,71 @@
+#include "test_simplecmdioclient.h"
+#include "simplecmdioclient.h"
+
+#include <timemachinefortest.h>
+#include <timerfactoryqtfortest.h>
+#include <QSignalSpy>
+#include <QTest>
+
+QTEST_MAIN(test_simplecmdioclient)
+
+constexpr int testTimeout = 2000;
+constexpr int testPort = 4712;
+
+
+void test_simplecmdioclient::init()
+{
+    TimerFactoryQtForTest::enableTest();
+    TimeMachineForTest::reset();
+}
+
+void test_simplecmdioclient::cleanup()
+{
+    m_simpleServer = nullptr;
+    TimeMachineObject::feedEventLoop();
+}
+
+void test_simplecmdioclient::cmdInvalidServer()
+{
+    SimpleCmdIoClient client("127.0.0.1", testPort, testTimeout);
+    QSignalSpy spy(&client, &SimpleCmdIoClient::sigCmdFinish);
+    client.startCmd("foo");
+
+    TimeMachineForTest::getInstance()->processTimers(testTimeout);
+    QCOMPARE(spy.count(), 1);
+    QCOMPARE(spy[0][0], false);
+}
+
+void test_simplecmdioclient::cmdInvalid()
+{
+    setupServer();
+    m_simpleServer->addCmd("foo", CmdParamTypeIdList(), TestCmdIoCompleteServer::RESULT_OK, "42");
+
+    SimpleCmdIoClient client("127.0.0.1", testPort, testTimeout);
+    QSignalSpy spy(&client, &SimpleCmdIoClient::sigCmdFinish);
+    client.startCmd("bar");
+
+    TimeMachineForTest::getInstance()->processTimers(testTimeout);
+    QCOMPARE(spy.count(), 1);
+    QCOMPARE(spy[0][0], false);
+}
+
+void test_simplecmdioclient::cmdOk()
+{
+    setupServer();
+    QString testCmd = "TestCmd";
+    m_simpleServer->addCmd(testCmd, CmdParamTypeIdList(), TestCmdIoCompleteServer::RESULT_OK, "42");
+
+    SimpleCmdIoClient client("127.0.0.1", testPort, testTimeout);
+    QSignalSpy spy(&client, &SimpleCmdIoClient::sigCmdFinish);
+    client.startCmd(testCmd);
+
+    TimeMachineForTest::getInstance()->processTimers(testTimeout);
+    QCOMPARE(spy.count(), 1);
+    QCOMPARE(spy[0][0], true);
+}
+
+void test_simplecmdioclient::setupServer()
+{
+    m_simpleServer = std::make_unique<TestCmdIoCompleteServer>(testPort);
+}
+
