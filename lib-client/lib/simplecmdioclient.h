d7f1000c2a423572e1769f64e629d26ec23b25e3
mdoerr2s <mika.doerr@smail.inf.h-brs.de>
2024-06-19 11:10:27 +0200

    SimpleCmdIoClient: Add test and functionality to send commands
    Signed-off-by: mdoerr2s <mika.doerr@smail.inf.h-brs.de>

--- lib-client/lib/simplecmdioclient.h
+++ lib-client/lib/simplecmdioclient.h
@@ -1,18 +1,24 @@
 #ifndef SIMPLECMDIOCLIENT_H
 #define SIMPLECMDIOCLIENT_H
 
+#include <taskcontainersequence.h>
 #include <QObject>
+#include <QTcpSocket>
 
 class SimpleCmdIoClient : public QObject
 {
     Q_OBJECT
 public:
-    explicit SimpleCmdIoClient(int portNo);
+    explicit SimpleCmdIoClient(QString ip, int portNo, int timeout);
     void startCmd(QString cmd);
 signals:
     void sigCmdFinish(bool ok);
 private:
+    QString m_ip;
     int m_portNo;
+    int m_timeout;
+    std::unique_ptr<TaskContainerInterface> m_tasks;
+    std::shared_ptr<QTcpSocket> m_socket;
 };
 
 #endif // SIMPLECMDIOCLIENT_H
---
+++ lib-client/tests/test_simplecmdioclient.h
@@ -0,0 +1,24 @@
+#ifndef TEST_SIMPLECMDIOCLIENT_H
+#define TEST_SIMPLECMDIOCLIENT_H
+
+#include "testcmdiocompleteserver.h"
+
+#include <QObject>
+#include <memory>
+
+class test_simplecmdioclient : public QObject
+{
+    Q_OBJECT
+private slots:
+    void init();
+    void cleanup();
+    void cmdInvalidServer();
+    void cmdInvalid();
+    void cmdOk();
+
+private:
+    void setupServer();
+    std::unique_ptr<TestCmdIoCompleteServer> m_simpleServer;
+};
+
+#endif // TEST_SIMPLECMDIOCLIENT_H
